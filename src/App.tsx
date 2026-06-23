529bbd28897488124e1545b51182cba453c54800
dmitriy <dmitriy.lunin777@gmail.com>
2026-06-23 12:18:59 +0300

    добавил в Footer фильтры и localStorage для сохранения состояния

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { useReducer } from 'react';
 import { todoReducer, initialState } from './reducers/todoReducer';
+import { loadState, useLocalStorage } from './hooks/UseLocalStorage';
 import { Header } from './components/Header/Header';
 import { TodoList } from './components/TodoList/TodoList';
+import { Footer } from './components/Footer/Footer';
 
 const App = () => {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const saved = loadState();
+  const [state, dispatch] = useReducer(todoReducer, saved || initialState);
+
+  useLocalStorage(state);
 
   return (
     <div>
       <Header dispatch={dispatch} />
       <TodoList state={state} dispatch={dispatch} />
+      <Footer state={state} dispatch={dispatch} />
     </div>
   );
 };
